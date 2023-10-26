8750ac5529b595e14ec6a3ccc83e7415283bf4e2
r-takahashi <147617856+r-takahashi-dev@users.noreply.github.com>
2023-10-26 14:01:02 +0000

    Update index.mjs Section5_34

--- src/index.mjs
+++ src/index.mjs
@@ -14,10 +14,26 @@ const onClickAdd = () => {
     // タグ生成
     const li = document.createElement("li");
     li.innerText = inputText;
+
+    // button（完了）タグ生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    });
+    
+    // button（削除）タグ生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+        alert("削除");
+    })
     
     // divタグの子要素に各変数を設定
     div.appendChild(li);
-    
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+
     // 未完了のリストに追加
     document.getElementById("incomplete-list").appendChild(div);
 }
